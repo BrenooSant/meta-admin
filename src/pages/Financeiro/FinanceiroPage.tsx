0747cdd9848f296fa9633f36a524ae069c187037
Breno Santos <brenoosant19@gmail.com>
2026-03-20 00:50:36 -0300

    .

--- src/pages/Financeiro/FinanceiroPage.tsx
+++ src/pages/Financeiro/FinanceiroPage.tsx
@@ -1,7 +1,7 @@
 import { useState, useRef, useEffect } from "react";
-import { FinanceiroQuadras } from "../../pages/Financeiro/componentes/FinanceiroQuadras";
-import { FinanceiroAcademia } from "../../pages/Financeiro/componentes/FinanceiroAcademia";
-import { FinanceiroGeral } from "../../pages/Financeiro/componentes/FinanceiroGeral";
+import { FinanceiroQuadras } from "./componentes/FinanceiroQuadras";
+import { FinanceiroAcademia } from "./componentes/FinanceiroAcademia";
+import { FinanceiroGeral } from "./componentes/FinanceiroGeral";
 
 type Filtro = "geral" | "academia" | "quadras";
 
